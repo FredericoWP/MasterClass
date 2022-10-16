b54ed4f3ec0ee7dbd432b778d1b90a72523597d6
FredericoWP <frederico.wallace@yahoo.com>
2022-10-16 11:32:14 -0300

    add desafio regra de 3

--- bin/masterclass.dart
+++ bin/masterclass.dart
@@ -3,6 +3,6 @@ import 'package:masterclass/masterclass.dart' as masterclass;
 void main(List<String> arguments) {
   print("Desafio sequencia de Fibonacci");
   print("R: ${masterclass.fibonacci(0, 10)}");
-  // print("Desafio regra de 3");
-  // print("R: ${masterclass.fibonacci(100)}");
+  print("Desafio regra de 3");
+  masterclass.regraDeTres(grupo1_1: 1.2, grupo1_2: 1.5, grupo2_2: 500);
 }
--- lib/masterclass.dart
+++ lib/masterclass.dart
@@ -16,3 +16,47 @@ List<int> fibonacci(int element, int length) {
   if (sequencia.length >= length) return sequencia;
   return fibonacci(sequencia.last, length);
 }
+
+regraDeTres({grupo1_1 = 'x', grupo1_2 = 'x', grupo2_1 = 'x', grupo2_2 = 'x'}) {
+  var x = [grupo1_1, grupo1_2, grupo2_1, grupo2_2];
+  double calc1 = 0.0;
+  double calc2 = 0.0;
+  if (x.where((e) => e == "x").toList().length > 1) {
+    print("deixe apenas 1 argumento nulo para definição do valor X");
+  } else {
+    print("${x[0]} * ${x[3]} = ${x[2]} * ${x[1]}");
+    if (x[0] == "x") {
+      print("${x[3]}${x[0]} = ${x[2]} * ${x[1]}");
+      calc1 = x[2] * x[1];
+      print("------------------------------------");
+      print("${x[0]} = $calc1 / ${x[3]}");
+      calc2 = calc1 / x[3];
+      print("------------------------------------");
+      print("${x[0]} = $calc2");
+    } else if (x[1] == "x") {
+      print("${x[2]}${x[1]} = ${x[0]} * ${x[3]}");
+      calc1 = x[0] * x[3];
+      print("------------------------------------");
+      print("${x[1]} = $calc1 / ${x[2]}");
+      calc2 = calc1 / x[2];
+      print("------------------------------------");
+      print("${x[1]} = $calc2");
+    } else if (x[2] == "x") {
+      print("${x[1]}${x[2]} = ${x[0]} * ${x[3]}");
+      calc1 = x[0] * x[3];
+      print("------------------------------------");
+      print("${x[2]} = $calc1 / ${x[1]}");
+      calc2 = calc1 / x[1];
+      print("------------------------------------");
+      print("${x[2]} = $calc2");
+    } else {
+      print("${x[0]}${x[3]} = ${x[2]} * ${x[1]}");
+      calc1 = x[2] * x[1];
+      print("------------------------------------");
+      print("${x[3]} = $calc1 / ${x[0]}");
+      calc2 = calc1 / x[0];
+      print("------------------------------------");
+      print("${x[3]} = $calc2");
+    }
+  }
+}
