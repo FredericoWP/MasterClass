f881928dc41275d86ae30bd2cb87a5974c1b518f
Frederico Pereira <113041949+FredericoWP@users.noreply.github.com>
2022-10-19 13:44:58 -0300

    Update masterclass.dart
    add Validador de CPF

--- bin/masterclass.dart
+++ bin/masterclass.dart
@@ -5,5 +5,6 @@ void main(List<String> arguments) {
   // print("R: ${masterclass.fibonacci(0, 10)}");
   // print("Desafio regra de 3");
   // masterclass.regraDeTres(grupo1_1: 1.2, grupo1_2: 1.5, grupo2_2: 500);
-  masterclass.checkCard("5209 5035 8592 2645");
+  // masterclass.checkCard("5209 5035 8592 2645");
+  masterclass.validaCPF("87470038809");
 }
