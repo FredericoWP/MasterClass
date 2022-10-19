c6398a39eeac36505f0143c263b74bff49b2d74a
FredericoWP <frederico.wallace@yahoo.com>
2022-10-18 21:21:10 -0300

    Calc Mod10

--- bin/masterclass.dart
+++ bin/masterclass.dart
@@ -1,8 +1,9 @@
 import 'package:masterclass/masterclass.dart' as masterclass;
 
 void main(List<String> arguments) {
-  print("Desafio sequencia de Fibonacci");
-  print("R: ${masterclass.fibonacci(0, 10)}");
-  print("Desafio regra de 3");
-  masterclass.regraDeTres(grupo1_1: 1.2, grupo1_2: 1.5, grupo2_2: 500);
+  // print("Desafio sequencia de Fibonacci");
+  // print("R: ${masterclass.fibonacci(0, 10)}");
+  // print("Desafio regra de 3");
+  // masterclass.regraDeTres(grupo1_1: 1.2, grupo1_2: 1.5, grupo2_2: 500);
+  masterclass.checkCard("5209 5035 8592 2645");
 }
--- lib/masterclass.dart
+++ lib/masterclass.dart
@@ -60,3 +60,40 @@ regraDeTres({grupo1_1 = 'x', grupo1_2 = 'x', grupo2_1 = 'x', grupo2_2 = 'x'}) {
     }
   }
 }
+
+checkCard(String ncard) {
+  // 4916 6418 5936 9080 - valido
+  // 5419 8250 0346 1210 - nValido
+
+  bool resp = false;
+  var result1 = <int>[];
+  var result2 = 0;
+  var sequencia =
+      ncard.replaceAll(' ', '').split('').map((e) => int.parse(e)).toList();
+  //print(sequencia);
+
+  if (ncard.length < 16) {
+    print("Você precisa informar os 16 digitos do cartão");
+  } else {
+    for (var i = 0; i < sequencia.length - 1; i++) {
+      int n = sequencia[i];
+      result1.add(i % 2 == 0 ? (n) * 2 : (n));
+    }
+
+    result1.forEach((e) {
+      if (e > 9) {
+        var nDouble = e.toString().split('').map((e) {
+          result2 += int.parse(e);
+        }).toList();
+      } else {
+        result2 += e;
+      }
+    });
+
+    resp = ((result2 % 10)) == sequencia[sequencia.length - 1];
+
+    print("$result1");
+    print("$result2");
+    print(resp ? "Cartão válido" : "Cartão inválido");
+  }
+}
